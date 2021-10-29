d224ef0121db38ac2b3766500363ddfce1bdf0e6
Kevin Murray <foss@kdmurray.id.au>
2021-10-29 18:39:20 +1100

    add test for xrender

--- tests/testthat/test-xspin.R
+++ tests/testthat/test-xspin.R
@@ -1,15 +1,25 @@
-do_test = function(input) {
-    res = xspin(input, output=paste0(input, ".got"))
-    expect = readLines(paste0(input, ".expect"))
-    got = readLines(paste0(input, ".got"))
 
-    expect_equal(expect, got)
-    expect_equal(paste(expect, collapse="\n"), res)
-}
+test_that("xspin produces correct output", {
+    do_test = function(input) {
+        res = xspin(input, output=paste0(input, ".got"))
+        expect = readLines(paste0(input, ".expect"))
+        got = readLines(paste0(input, ".got"))
 
-test_that("xspin compiles", {
+        expect_equal(expect, got)
+        expect_equal(paste(expect, collapse="\n"), res)
+    }
     
     do_test("data/simple.sh")
     do_test("data/simple.py")
+
+})
+
+test_that("xrender works", {
+    do_test("data/simple.sh")
+    do_test("data/simple.py")
+
+    if (file.exists("data/simple.html")) unlink("data/simple.html")
+    xrender("data/simple.sh")
+    expect_true(file.exists("data/simple.html"))
 
 })
